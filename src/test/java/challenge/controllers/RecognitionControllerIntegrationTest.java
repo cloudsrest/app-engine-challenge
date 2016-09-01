b718877a9d16cc21eb1e3121f309bf00d7c7aa42
irieb <irieb88@gmail.com>
2016-09-01 09:45:22 -0700

    fixed unit-test

--- src/test/java/challenge/controllers/RecognitionControllerIntegrationTest.java
+++ src/test/java/challenge/controllers/RecognitionControllerIntegrationTest.java
@@ -5,6 +5,8 @@ import challenge.dto.Recognition;
 import challenge.dto.RecognitionTypeEnum;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
@@ -17,6 +19,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -24,18 +27,22 @@ import static org.junit.Assert.assertFalse;
 @IntegrationTest("server.port=9000")
 public class RecognitionControllerIntegrationTest extends BaseRestTest {
 
-	// FIXME - temporary ignore so that build can pass
-	@org.junit.Ignore
+    @Before
+    public void setup() {
+        deleteAllRecognitions();
+    }
+
+    @After
+    public void tearDown() {
+        deleteAllRecognitions();
+    }
+
     @Test
     public void testCreateRecognitions() throws JsonProcessingException {
         Recognition recognition1 = new Recognition("111","222", RecognitionTypeEnum.CREATIVITY.toString(),"xxx","zzz");
         Recognition recognition2 = new Recognition("333","444", RecognitionTypeEnum.HARD_WORK.toString(),"iii","ppp");
 
-        ObjectMapper mapper = new ObjectMapper();
-        String s = mapper.writeValueAsString(recognition1);
-        System.out.println("json = " + s);
-
-        restTemplate.postForEntity("http://localhost:9000/recognitions", recognition1, String.class);
+        createRecognition(recognition1);
 
         Recognition[] forNow = restTemplate.getForObject("http://localhost:9000/recognitions", Recognition[].class);
         List<Recognition> recognitions = Arrays.asList(forNow);
@@ -43,12 +50,31 @@ public class RecognitionControllerIntegrationTest extends BaseRestTest {
         assertEquals(1, recognitions.size());
         assertEquals(recognition1, recognitions.get(0));
 
-        restTemplate.postForEntity("http://localhost:9000/recognitions", recognition2, String.class);
+        createRecognition(recognition2);
 
-        forNow = restTemplate.getForObject("http://localhost:9000/recognitions", Recognition[].class);
-        recognitions = Arrays.asList(forNow);
+        recognitions = getRecognitions();
         assertFalse(recognitions.isEmpty());
         assertEquals(2, recognitions.size());
+        assertEquals(recognition2, recognitions.get(1));
+
+        deleteAllRecognitions();
+
+        recognitions = getRecognitions();
+        assertTrue(recognitions.isEmpty());
+    }
+
+    private void createRecognition(Recognition recognition2) {
+        restTemplate.postForEntity("http://localhost:9000/recognitions", recognition2, String.class);
+    }
+
+    private void deleteAllRecognitions() {
+        restTemplate.delete("http://localhost:9000/recognitions");
+    }
+
+    private List<Recognition> getRecognitions() {
+        Recognition[] forNow;
+        forNow = restTemplate.getForObject("http://localhost:9000/recognitions", Recognition[].class);
+        return Arrays.asList(forNow);
     }
 
 }
