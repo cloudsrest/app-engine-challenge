ec24875f0c02fd247168be783ecaffe8e5845cd6
irieb <irieb88@gmail.com>
2016-09-02 12:28:19 -0700

    missed this file... jeez, sorry about the mess

---
+++ src/main/java/challenge/model/Team.java
@@ -0,0 +1,40 @@
+package challenge.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy="team")
+    private List<User> users; 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
