1de0787eb0546f0369703b1be707580289e3894b
adrianyacub <adrianyacub@yahoo-inc.com>
2016-09-05 16:39:24 -0700

    need @ResponseBody

--- src/main/java/challenge/controller/MainController.java
+++ src/main/java/challenge/controller/MainController.java
@@ -4,13 +4,14 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET, produces=MediaType.TEXT_HTML_VALUE)
-    public String index() {
+    public @ResponseBody String index() {
         return "Welcome to SpringBoot123! (playing with travis)";
     }
 
