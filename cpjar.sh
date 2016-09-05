9b51688b5d5c7c13bb0a1de408ec61ef62433c1a
Joerg Weingarten <joerg@yahoo-inc.com>
2016-09-05 14:53:30 -0700

    Copy the app.yaml

--- cpjar.sh
+++ cpjar.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir target/appengine-staging
+cp app.yaml ./target/appengine-staging
 mvn package -Dmaven.test.skip=true
 ls -all ./target
-cp ./target/spring-boot*.jar target/appengine-staging
+cp ./target/spring-boot*.jar ./target/appengine-staging
+ls -all ./target/appengine-staging
 
 
