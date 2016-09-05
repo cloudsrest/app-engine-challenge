229f81ced872a3e80dbbb8cd9b2c2d42f613609f
Joerg Weingarten <joerg@yahoo-inc.com>
2016-09-05 14:45:45 -0700

    Some debug info

--- cpjar.sh
+++ cpjar.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir target/appengine-staging
-cp target/spring-boot*.jar target/appengine-staging
+ls -all ./target
+cp ./target/spring-boot*.jar target/appengine-staging
 
 
