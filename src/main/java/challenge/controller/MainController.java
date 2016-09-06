3495a3a9dca2ae17831a8d78092d46bd648b6a8c
adrianyacub <adrianyacub@yahoo-inc.com>
2016-09-05 17:33:39 -0700

    adds encryption of client-secret, builds only on master

--- src/main/java/challenge/controller/MainController.java
+++ src/main/java/challenge/controller/MainController.java
@@ -4,14 +4,15 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET, produces=MediaType.TEXT_HTML_VALUE)
-    public String index() {
-        return "Welcome to SpringBoot123!";
+    public @ResponseBody String index() {
+        return "Welcome to SpringBoot! (as seen via Travis)";
     }
 
 }
