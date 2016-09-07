c35b28fe052627a577882956b5293df1cd3a30ac
irieb <irieb88@gmail.com>
2016-09-07 11:04:49 -0700

    FCE-29 - OAuth2. In-memory DB, refactored test, support for env switching

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `oauth_access_token`;
-DROP TABLE IF EXISTS `oauth_refresh_token`;
+DROP TABLE IF EXISTS oauth_access_token;
+DROP TABLE IF EXISTS oauth_refresh_token;
 
 CREATE TABLE oauth_access_token (
   token_id VARCHAR(256) DEFAULT NULL,
@@ -13,5 +13,6 @@ CREATE TABLE oauth_access_token (
 CREATE TABLE oauth_refresh_token (
   token_id VARCHAR(256) DEFAULT NULL,
   token BLOB,
+  authentication BLOB
   authentication BLOB
 );
--- src/test/java/challenge/integration/UserControllerIntegrationTest.java
+++ src/test/java/challenge/integration/UserControllerIntegrationTest.java
@@ -4,22 +4,24 @@ import challenge.dto.ErrorDTO;
 import challenge.dto.TokenDTO;
 import challenge.dto.UserDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
