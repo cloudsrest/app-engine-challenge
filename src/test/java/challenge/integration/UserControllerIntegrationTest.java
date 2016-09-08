0b712ab68270a2dce816ad42db24d9377d124f6e
irieb <irieb88@gmail.com>
2016-09-08 11:19:51 -0700

    FCE-36 - added controller test to assert pass on master

--- src/test/java/challenge/integration/UserControllerIntegrationTest.java
+++ src/test/java/challenge/integration/UserControllerIntegrationTest.java
@@ -4,23 +4,19 @@ import challenge.dto.ErrorDTO;
 import challenge.dto.TokenDTO;
 import challenge.dto.UserDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 @RunWith(SpringRunner.class)
