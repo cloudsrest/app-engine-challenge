aa3d3cbd866a1a4c86c63bf7bf4deffbe79c0968
Cherie Youn <cyoon@yahoo.com>
2016-09-09 13:15:32 -0700

    FCE-36 more test on no_stop_previous_version

--- src/main/java/challenge/controller/HealthController.java
+++ src/main/java/challenge/controller/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/public/health")
 public class HealthController {
 
-    public static final String STATUS = "Up and Running! staging/prod env.";
+    public static final String STATUS = "Up and Running! V2!";
 
     @RequestMapping(method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody Health health() {
