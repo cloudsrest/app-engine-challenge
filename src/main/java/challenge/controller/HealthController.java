a6fc1ace3670be48416de59d12ef881b30e5038b
Cherie Youn <cyoon@yahoo.com>
2016-09-09 00:03:58 -0700

    FCE-36 give version for 'prod'

--- src/main/java/challenge/controller/HealthController.java
+++ src/main/java/challenge/controller/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/public/health")
 public class HealthController {
 
-    public static final String STATUS = "Up and Running! CI/CD pipeline enabled.";
+    public static final String STATUS = "Up and Running! staging/prod env.";
 
     @RequestMapping(method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody Health health() {
